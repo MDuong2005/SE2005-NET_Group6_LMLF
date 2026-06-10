e65669bc2b99757f18337ac514caaa1a2e09d4c8
MDuong <maid88391@gmail.com>
2026-06-10 23:37:59 +0700

    Implement authentication module

--- G6_LMLF_System/src/java/model/Role.java
+++ G6_LMLF_System/src/java/model/Role.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author maid8
- */
 public class Role {
-    
+
+    private long roleId;
+    private String roleName;
+    private String description;
+
+    public Role() {
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
