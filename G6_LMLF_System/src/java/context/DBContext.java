ba710e86b735208b515c75f17e606f47c4fb80e3
MDuong <maid88391@gmail.com>
2026-06-10 16:49:18 +0700

    initialize project architecture and base structure

---
+++ G6_LMLF_System/src/java/context/DBContext.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maid8
+ */
+public class DBContext {
+    
+}
