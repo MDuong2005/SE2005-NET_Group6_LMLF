0499af84c9c4cf4c2d07bc22312b758faeeff869
Bùi Minh Long <longbmhe182468@fpt.edu.vn>
2026-06-16 21:16:26 +0700

    Update CourseDAO.java

--- G6_LMLF_System/src/java/dao/CourseDAO.java
+++ G6_LMLF_System/src/java/dao/CourseDAO.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author maid8
- */
-public class CourseDAO {
-    
+import context.DBContext;
+import model.Course;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDAO extends DBContext {
+
+    public List<Course> getAllCourses() {
+        List<Course> list = new ArrayList<>();
+        String sql = "SELECT * FROM courses ORDER BY code";
+        try {
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Course course = new Course();
+                    course.setCourseId(rs.getLong("course_id"));
+                    course.setCode(rs.getString("code"));
+                    course.setName(rs.getString("name"));
+                    course.setCredits(rs.getInt("credits"));
+                    course.setCreatedAt(rs.getTimestamp("created_at"));
+                    list.add(course);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
