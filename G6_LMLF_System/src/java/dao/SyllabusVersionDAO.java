fada48781e2cf955ed16999b8e63ad339768d782
dinhanh19102005 <dinhanh19102005@gmail.com>
2026-06-22 13:37:59 +0700

    Publish Version /Archive Previous

--- G6_LMLF_System/src/java/dao/SyllabusVersionDAO.java
+++ G6_LMLF_System/src/java/dao/SyllabusVersionDAO.java
@@ -51,4 +51,110 @@ public class SyllabusVersionDAO extends DBContext {
             return false;
         }
     }
+
+    public boolean archiveCurrentPublishedVersion(long syllabusId) {
+        String sql = """
+        UPDATE syllabus_version_publications
+        SET status = 'ARCHIVED',
+            archived_at = GETDATE()
+        WHERE syllabus_id = ?
+          AND status = 'PUBLISHED'
+    """;
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setLong(1, syllabusId);
+
+            ps.executeUpdate();
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean publishVersion(long versionId, long publisherId) {
+        String getVersionSql = """
+        SELECT syllabus_id, version_number
+        FROM syllabus_versions
+        WHERE version_id = ?
+          AND status = 'APPROVED'
+    """;
+
+        String insertPublicationSql = """
+        INSERT INTO syllabus_version_publications
+        (version_id, syllabus_id, status, published_by, published_at)
+        VALUES (?, ?, 'PUBLISHED', ?, GETDATE())
+    """;
+
+        String updateSyllabusSql = """
+        UPDATE syllabuses
+        SET status = 'PUBLISHED',
+            current_version = ?,
+            updated_at = GETDATE(),
+            updated_by = ?
+        WHERE syllabus_id = ?
+    """;
+
+        try {
+            connection.setAutoCommit(false);
+
+            PreparedStatement getPs = connection.prepareStatement(getVersionSql);
+            getPs.setLong(1, versionId);
+
+            ResultSet rs = getPs.executeQuery();
+
+            if (!rs.next()) {
+                connection.rollback();
+                return false;
+            }
+
+            long syllabusId = rs.getLong("syllabus_id");
+            String versionNumber = rs.getString("version_number");
+
+            boolean archived = archiveCurrentPublishedVersion(syllabusId);
+
+            if (!archived) {
+                connection.rollback();
+                return false;
+            }
+
+            PreparedStatement insertPs
+                    = connection.prepareStatement(insertPublicationSql);
+
+            insertPs.setLong(1, versionId);
+            insertPs.setLong(2, syllabusId);
+            insertPs.setLong(3, publisherId);
+            insertPs.executeUpdate();
+
+            PreparedStatement updatePs
+                    = connection.prepareStatement(updateSyllabusSql);
+
+            updatePs.setString(1, versionNumber);
+            updatePs.setLong(2, publisherId);
+            updatePs.setLong(3, syllabusId);
+            updatePs.executeUpdate();
+
+            connection.commit();
+            return true;
+
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
